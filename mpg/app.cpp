dffb46f613bbfcbb19b6543f8a71934e49cf2865
Oleg Akhmetshin <ahm@bk.ru>
2020-10-06 00:53:36 +0500

    Рефаакторинг - разносим PGConnection по файлам

--- mpg/app.cpp
+++ mpg/app.cpp
@@ -1,53 +1,13 @@
+#include <windows.h>
 #include <iostream>
 #include <memory>
 #include <string>
 #include <libpq-fe.h>
-
-class PGConnection
-{
-private:
-
-    std::string dbhost = "localhost";
-    std::string dbport = "5432";
-    std::string dbname = "testdb";
-    std::string dbuser = "postgres";
-    std::string dbpass = "1";
-
-    std::shared_ptr<PGconn> connection;
-
-public:
-    PGConnection()
-    {
-        connection.reset(PQsetdbLogin(
-                               dbhost.c_str(),
-                               dbport.c_str(),
-                               nullptr,
-                               nullptr,
-                               dbname.c_str(),
-                               dbuser.c_str(),
-                               dbpass.c_str()),
-                           &PQfinish);
-
-        if (PQstatus(connection.get()) != CONNECTION_OK)
-        {
-            throw std::runtime_error(PQerrorMessage(connection.get()));
-        }
-    }
-
-    int exec(const char *query)
-    {
-        PGresult *res = PQexec(connection.get(), query);
-        if (PQresultStatus(res) == PGRES_FATAL_ERROR){
-            throw std::runtime_error(PQerrorMessage(connection.get()));
-        }
-        int result_n = PQntuples(res);
-        PQclear(res);
-        return result_n;
-    }
-};
+#include "pg_connection.h"
 
 int main()
 {
+    SetConsoleOutputCP(1251);
     PGConnection* conn = new PGConnection();
     std::cout << conn->exec("SELECT 1+1");
     delete conn;
